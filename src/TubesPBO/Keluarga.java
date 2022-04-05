93db5f8d0a5ba77d1c4d9b7c30ebc3bde8104c6b
naufalchairurrijal <naufalalifaza@student.telkomuniversity.ac.id>
2022-04-05 21:09:28 +0700

    menambahkan class keluarga

---
+++ src/TubesPBO/Keluarga.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TubesPBO;
+
+/**
+ *
+ * @author HP
+ */
+public class Keluarga extends Base {
+    private int id_keluarga;
+    private int kepala_keluarga;
+    
+    public Keluarga(int id_keluarga, int kepala_keluarga){
+        this.id_keluarga = id_keluarga;
+        this.kepala_keluarga = kepala_keluarga;
+    }
+
+    @Override
+    void addData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void updateData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    void deleteData() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

1799cbc5b570d6096aaf5357623786f1536336ea
Zahrandi Rusrizal <Zahrandi@student.telkomuniversity.ac.id>
2022-04-05 21:01:45 +0700

    Menambahkan Class User dan Class Pengungsian

---
+++ src/TubesPBO/User.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TubesPBO;
+
+/**
+ *
+ * @author Sabre
+ */
+public class User extends Base{
+    private int id_user;
+    private int profil_user;
+    private int role;
+    private String username;
+    private String email;
+    private String password;
+
+    @Override
+    void addData() {
+
+    }
+
+    @Override
+    void updateData() {
+
+    }
+
+    @Override
+    void deleteData() {
+
+    }
+    
+    
+}

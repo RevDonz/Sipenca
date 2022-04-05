502f6c063f04425ec946637cd1ea284c53a15be9
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-04-05 21:28:04 +0700

    Added Kebutuhan & Pengelola

---
+++ src/TubesPBO/Kebutuhan.java
@@ -0,0 +1,28 @@
+package TubesPBO;
+
+public class Kebutuhan extends Base {
+    private int id_kebutuhan;
+    private int keluarga;
+    private String nama_barang;
+    private String satuan;
+    private int jumlah;
+    private boolean adalah_pokok;
+
+    @Override
+    void addData() {
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
+    }
+
+    @Override
+    void updateData() {
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
+    }
+
+    @Override
+    void deleteData() {
+        throw new UnsupportedOperationException("Not supported yet."); // To change body of generated methods, choose
+                                                                       // Tools | Templates.
+    }
+}

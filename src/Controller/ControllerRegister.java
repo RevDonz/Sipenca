f00fada9f83806e885316f14d2348e6ef3d77030
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-06-26 18:06:11 +0700

    added register DAO & controller

---
+++ src/Controller/ControllerRegister.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import DAO.RegisterDAO;
+import View.RegisterView;
+
+public class ControllerRegister {
+    RegisterView frame;
+    RegisterDAO rd;
+
+    public ControllerRegister() {
+        this.frame = frame;
+        rd = new RegisterDAO();
+    }
+
+    public void 
+}

2833fab83ac4eef1de3a44e4a4396e7aa6c53d1a
rpadliwinata <rpadliwinata@student.telkomuniversity.ac.id>
2022-06-26 22:30:35 +0700

    added register

--- src/Controller/ControllerRegister.java
+++ src/Controller/ControllerRegister.java
@@ -2,6 +2,8 @@ package Controller;
 
 import DAO.RegisterDAO;
 import View.RegisterView;
+import View.ViewLogin;
+import javax.swing.JOptionPane;
 
 public class ControllerRegister {
     RegisterView frame;
@@ -12,5 +14,23 @@ public class ControllerRegister {
         rd = new RegisterDAO();
     }
 
-    public void 
+    public void UserRegister() {
+        int role = 0;
+        
+        String username = frame.getInputPassword().getText();
+        String email = frame.getInputEmail().getText();
+        String password = frame.getInputPassword().getText();
+        String s_role = frame.getInputRole().getSelection().getActionCommand();
+        if (s_role == "Warga") {
+            role = 2;
+        } else if ((s_role == "Sukarelawan") || (s_role == "Petugas")) {
+            role = 3;
+        }
+        if (rd.register(username, password, email, role)) {
+            JOptionPane.showMessageDialog(null, "Berhasil Register!", "Data Recorded", JOptionPane.INFORMATION_MESSAGE);
+            new ViewLogin().setVisible(true);
+        } else {
+            JOptionPane.showMessageDialog(null, "Error Saat Menyimpan data", "SQL Error", JOptionPane.WARNING_MESSAGE);
+        }
+    }
 }
